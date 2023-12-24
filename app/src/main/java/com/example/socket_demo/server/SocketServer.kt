294fd87e3c3a884d6c12c25de2d1550b5c1e8d7b
darkangel20156 <darkangel20156@gmail.com>
2023-12-24 14:14:16 +0700

    fix code for multiple access to server

--- app/src/main/java/com/example/socket_demo/server/SocketServer.kt
+++ app/src/main/java/com/example/socket_demo/server/SocketServer.kt
@@ -7,7 +7,7 @@ import java.io.OutputStream
 import java.net.ServerSocket
 import java.net.Socket
 import java.util.concurrent.Executors
-import kotlin.math.log
+
 /**
  * Socket server
  */
@@ -17,14 +17,12 @@ object SocketServer {
 
     private const val SOCKET_PORT = 9527
 
-    private var socket: Socket? = null
     private var serverSocket: ServerSocket? = null
 
     private lateinit var mCallback: ServerCallback
 
-    private lateinit var outputStream: OutputStream
+    private val clientThreads = mutableListOf<ServerThread>()
 
-    var result = true
     /**
      * Start service
      */
@@ -33,67 +31,57 @@ object SocketServer {
         Thread {
             try {
                 serverSocket = ServerSocket(SOCKET_PORT)
-                while (result) {
-                    socket = serverSocket?.accept()
-                    mCallback.otherMsg("${socket?.inetAddress} to connected")
-                    ServerThread(socket!!, mCallback).start()
+                while (true) {
+                    val socket = serverSocket?.accept()
+                    socket?.let {
+                        mCallback.otherMsg("${socket.inetAddress} connected")
+                        val clientThread = ServerThread(socket, mCallback)
+                        clientThreads.add(clientThread)
+                        clientThread.start()
+                    }
                 }
             } catch (e: IOException) {
                 e.printStackTrace()
-                result = false
             }
         }.start()
-        return result
+        return true
     }
 
     /**
      * Close service
      */
     fun stopServer() {
-        socket?.apply {
-            shutdownInput()
-            shutdownOutput()
-            close()
-        }
         serverSocket?.close()
+        for (clientThread in clientThreads) {
+            clientThread.stopThread()
+        }
+        clientThreads.clear()
     }
 
     /**
-     *Send to client
+     * Send to all clients
      */
-    fun sendToClient(msg: String) {
-        Thread {
-            if (socket!!.isClosed) {
-                Log.e(TAG, "sendToClient: Socket is closed")
-                return@Thread
-            }
-            outputStream = socket!!.getOutputStream()
-            try {
-                outputStream.write(msg.toByteArray())
-                outputStream.flush()
-                mCallback.otherMsg("toClient: $msg")
-                Log.d(TAG, "Sent to client successfully")
-            } catch (e: IOException) {
-                e.printStackTrace()
-                Log.e(TAG, "Failed to send message to client")
-            }
-        }.start()
+    fun sendToAllClients(msg: String) {
+        for (clientThread in clientThreads) {
+            clientThread.sendToClient(msg)
+        }
     }
 
-    class ServerThread(private val socket: Socket, private val callback: ServerCallback):
+    class ServerThread(private val socket: Socket, private val callback: ServerCallback) :
         Thread() {
 
+        private lateinit var outputStream: OutputStream
+        private var running = true
+
         override fun run() {
-            val inputStream: InputStream?
             try {
-                inputStream = socket.getInputStream()
+                outputStream = socket.getOutputStream()
+                val inputStream = socket.getInputStream()
                 val buffer = ByteArray(1024)
-                var len : Int
+                var len = 0
                 var receiveStr = ""
-                if (inputStream.available() == 0) {
-                    Log.e(TAG, "inputStream.available() == 0")
-                }
-                while (inputStream.read(buffer).also { len = it } != -1) {
+
+                while (running && inputStream.read(buffer).also { len = it } != -1) {
                     receiveStr += String(buffer, 0, len, Charsets.UTF_8)
                     if (len < 1024) {
                         callback.receiveClientMsg(true, receiveStr)
@@ -102,9 +90,24 @@ object SocketServer {
                 }
             } catch (e: IOException) {
                 e.printStackTrace()
-                e.message?.let { Log.e("socket error", it) }
                 callback.receiveClientMsg(false, "")
             }
+        }
+
+        fun sendToClient(msg: String) {
+            try {
+                outputStream.write(msg.toByteArray())
+                outputStream.flush()
+                callback.otherMsg("toClient: $msg")
+            } catch (e: IOException) {
+                e.printStackTrace()
+                callback.receiveClientMsg(false, "")
+            }
+        }
+
+        fun stopThread() {
+            running = false
+            socket.close()
         }
     }
 }
